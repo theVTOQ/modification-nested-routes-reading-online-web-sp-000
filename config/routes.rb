91349ba551f8f9fcde0ab659a5110dc0a52b7203
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-10-02 17:52:51 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
     resources :posts, only: [:show, :index, :new]
   end
 
-  resources :posts, only: [:index, :create, :edit]
+  resources :posts, only: [:index, :create, :edit, :show]
 end
